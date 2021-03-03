acfbf43007984f103e642a08c71cda4db7a2d630
taliayit <34692076+taliayit@users.noreply.github.com>
2021-03-03 10:58:15 +0200

    create change image function

---
+++ script.js
@@ -0,0 +1,12 @@
+var images = ['banner_atar', '250X110668568400', 'paz_250x110103131473'];
+var i = 0;
+
+function change() {
+    var imgElement = document.getElementById("img");
+    imgElement.src = "../images/" + images[i] + ".jpg";
+    i > 1 ? i = 0 : i++;
+}
+
+window.onload = function() {
+    setInterval(change, 1500);
+};
